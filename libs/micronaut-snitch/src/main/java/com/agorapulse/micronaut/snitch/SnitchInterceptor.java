9a8c2de738b4163dfb86c2e943aa9b3fdb34cff2
Sergio del Amo <sergio.delamo@softamo.com>
2021-12-07 17:42:17 +0100

    refactor: use beanContext instead of applicationcontext (#7)
    * refactor: use beanContext instead of applicationcontext
    
    * Update SnitchInterceptor.java
    
    * removed unused import
    
    Co-authored-by: Vladimir Orany <vladimir@orany.cz>

--- libs/micronaut-snitch/src/main/java/com/agorapulse/micronaut/snitch/SnitchInterceptor.java
+++ libs/micronaut-snitch/src/main/java/com/agorapulse/micronaut/snitch/SnitchInterceptor.java
@@ -19,9 +19,8 @@ package com.agorapulse.micronaut.snitch;
 
 import io.micronaut.aop.MethodInterceptor;
 import io.micronaut.aop.MethodInvocationContext;
-import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.BeanContext;
 import io.micronaut.inject.qualifiers.Qualifiers;
-
 import javax.inject.Singleton;
 
 /**
@@ -30,18 +29,18 @@ import javax.inject.Singleton;
 @Singleton
 public class SnitchInterceptor implements MethodInterceptor<Object, Object> {
 
-    private final ApplicationContext applicationContext;
+    private final BeanContext beanContext;
 
-    public SnitchInterceptor(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
+    public SnitchInterceptor(BeanContext beanContext) {
+        this.beanContext = beanContext;
     }
 
     @Override
     public Object intercept(MethodInvocationContext<Object, Object> methodContext) {
         String configurationName = methodContext.getValue(Snitch.class, String.class).orElse(Snitch.DEFAULT_CONFIGURATION_NAME);
         boolean before = methodContext.getValue(Snitch.class, "before", Boolean.class).orElse(false);
-        SnitchService service = applicationContext.findBean(SnitchService.class, Qualifiers.byName(configurationName))
-                .orElseGet(() -> applicationContext.getBean(SnitchService.class));
+        SnitchService service = beanContext.findBean(SnitchService.class, Qualifiers.byName(configurationName))
+                .orElseGet(() -> beanContext.getBean(SnitchService.class));
         try {
             if (before) {
                 service.snitch();
